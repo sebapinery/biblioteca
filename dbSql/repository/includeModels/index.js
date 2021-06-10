3636721a16545db719f6e9e6ddf2b7714b2fa504
Seba <sebapinery@gmail.com>
2021-06-10 14:07:52 -0300

    Refactor Templates of includes

--- dbSql/repository/includeModels/index.js
+++ dbSql/repository/includeModels/index.js
@@ -1,50 +1,32 @@
-import { 
-    author, 
-    book, 
-    tag, 
-    category, 
-    quotes 
-} from '../../models';
+import { author, book, tag, category, quotes } from '../../models';
+
+import { attributesOff, timestampsOff } from '../excludeTemplates';
 
 export const authorsInclude = {
   model: author,
   as: 'author',
-  attributes: {
-    exclude: ['createdAt', 'updatedAt', 'deletedAt'],
-  },
+  attributes: timestampsOff,
 };
 
 export const categoryInclude = {
   model: category,
   as: 'category',
-  attributes: {
-    exclude: ['createdAt', 'updatedAt', 'deletedAt'],
-  },
+  attributes: timestampsOff,
 };
 export const bookInclude = {
   model: book,
   as: 'books',
-  attributes: {
-    exclude: ['createdAt', 'updatedAt', 'deletedAt'],
-  },
+  attributes: timestampsOff,
 };
 export const quotesInclude = {
   model: quotes,
   as: 'quotes',
-  attributes: {
-    exclude: ['createdAt', 'updatedAt', 'deletedAt'],
-  },
-  through: {
-    attributes: [],
-  },
+  attributes: timestampsOff,
+  through: attributesOff,
 };
 export const tagInclude = {
-    model: tag,
-    as: 'tags',
-    attributes: {
-      exclude: ['createdAt', 'updatedAt', 'deletedAt'],
-    },
-    through: {
-      attributes: [],
-    },
-  };
+  model: tag,
+  as: 'tags',
+  attributes: timestampsOff,
+  through: attributesOff,
+};
