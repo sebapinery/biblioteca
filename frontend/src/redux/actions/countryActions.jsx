b40ff1f26ce19d97b004fbde376fc118120edfd9
sebapinery <sebapinery@gmail.com>
2021-07-09 18:09:16 -0300

    snackbar alert created

--- frontend/src/redux/actions/countryActions.jsx
+++ frontend/src/redux/actions/countryActions.jsx
@@ -1,4 +1,8 @@
-import { GET_COUNTRIES_FAIL, GET_COUNTRIES_REQUEST, GET_COUNTRIES_SUCCESS } from '../contants';
+import {
+  GET_COUNTRIES_FAIL,
+  GET_COUNTRIES_REQUEST,
+  GET_COUNTRIES_SUCCESS,
+} from '../contants';
 
 import axios from 'axios';
 
@@ -7,6 +11,14 @@ export const getCountriesAction = async (dispatch) => {
 
   await axios
     .get('https://restcountries.eu/rest/v2/all')
-    .then((res) => dispatch({ type: GET_COUNTRIES_SUCCESS, payload: res.data }))
-    .catch(error => dispatch({ type: GET_COUNTRIES_FAIL, payload: error.message}))
+    .then((res) => dispatch({ 
+      type: GET_COUNTRIES_SUCCESS, 
+      payload: res.data 
+    }))
+    .catch((error) =>
+      dispatch({ 
+        type: GET_COUNTRIES_FAIL, 
+        payload: error.message 
+      })
+    );
 };
